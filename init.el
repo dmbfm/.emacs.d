f76be7b2fe5f268a79039987222325dd9a12f483
Daniel Fortes <daniel.mbfm@gmail.com>
2019-12-08 20:30:49 -0300

    Hippie-expand configuration.

--- init.el
+++ init.el
@@ -206,3 +206,16 @@
 
 (global-set-key (kbd "C-c l") 'org-store-link)
 (setq org-link-search-must-match-exact-headline nil)
+
+
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+	try-expand-dabbrev-all-buffers
+	try-expand-dabbrev-from-kill
+	try-complete-file-name-partially
+	try-complete-file-name
+	try-expand-all-abbrevs
+	try-expand-list
+	try-expand-line
+	try-complete-lisp-symbol-partially
+	try-complete-lisp-symbol))
