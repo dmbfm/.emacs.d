ad3987eece40b339d079f4b0cbeb014adeb81040
Daniel Fortes <daniel.mbfm@gmail.com>
2020-04-27 16:46:19 -0300

    Treemacs

---
+++ elpa/treemacs-icons-dired-20200205.548/treemacs-icons-dired-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-icons-dired" "20200205.548" "Treemacs icons for dired" '((treemacs "0.0") (emacs "25.2") (cl-lib "0.5")) :commit "f82d61bdcb4ce4b294a95628ce1d62857991df48" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
