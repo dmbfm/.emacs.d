ec8f3275f94439a425ce5901cea846fb2459829c
Daniel Fortes <daniel.mbfm@gmail.com>
2020-05-27 11:32:04 -0300

    tsx

--- init.el
+++ init.el
@@ -240,6 +240,14 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
 ;; (use-package tide
 ;;   :ensure t
 ;;   :after (typescript-mode company flycheck)
